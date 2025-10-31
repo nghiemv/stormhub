86d0e01549a9f1e57edf6bdadc8226b937a258de
Zach Herbert <zachary.herbert@mbakerintl.com>
2025-10-31 19:14:58 +0000

    updated runner

--- runners/run_sst.py
+++ runners/run_sst.py
@@ -35,13 +35,13 @@ if __name__ == "__main__":
     )
 
     # All Collection Args
-    start_date = "2000-01-01"
-    end_date = "2001-01-01"
+    start_date = "2000-04-01"
+    end_date = "2000-05-01"
     top_n_events = 10
 
     # Collection Args
     storm_duration_hours = 72
-    min_precip_threshold = 1.5
+    min_precip_threshold = 0.75
     storm_collection = new_collection(
         storm_catalog,
         start_date,
@@ -50,7 +50,8 @@ if __name__ == "__main__":
         min_precip_threshold,
         top_n_events,
         check_every_n_hours=24,
-        num_workers=10,
+        num_workers=30,
+        use_threads=True,
     )
     end_time = time.time()
     elapsed_minutes = round((end_time - start_time) / 60, 2)
