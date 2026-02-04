748e2ab746be068e0995d3f0f53ae1cebd5f2b9a
sray014 <stevenrayjanke@yahoo.com>
2026-02-04 17:08:03 -0500

    Add swe data asset option to gage collection

--- workflows/gages.py
+++ workflows/gages.py
@@ -1,4 +1,4 @@
-from stormhub.hydro.usgs.usgs import new_gage_collection, new_gage_catalog
+from stormhub.hydro.usgs.usgs import new_gage_collection, new_gage_catalog, add_ams_swe_to_gage_collection
 from stormhub.hydro.utils import find_gages_in_watershed
 from stormhub.logger import initialize_logger
 
@@ -18,4 +18,12 @@ if __name__ == "__main__":
 
     # gage_numbers = ["12105900", "12167000"] # test gages
     gage_numbers = find_gages_in_watershed(watershed, 15)
-    new_gage_collection(gage_catalog, gage_numbers, local_directory)
+    collection = new_gage_collection(gage_catalog, gage_numbers, local_directory)
+
+    # Optionally add average SWE data to each gage item for each AMS event
+    # add_ams_swe_to_gage_collection(
+    #     gage_collection = collection,
+    #     drainage_area_geojson_path = "path/to/drainage_areas.geojson",
+    #     swe_zarr_path = "s3://path/to/swe.zarr")
+
+
